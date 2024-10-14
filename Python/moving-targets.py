918313671d24951e64ab01bd6d7799ae6fbead10
Sleggen <fredriksknive@gmail.com>
2024-10-14 13:08:52 +0200

    Update moving-targets.py
    Sleggosaur gjorde det utenkelige, en trippel push.
    en ekte hverdags helt, og gjett om han har kappe

--- Python/moving-targets.py
+++ Python/moving-targets.py
@@ -4,7 +4,7 @@ Created on Mon Oct 14 12:30:42 2024
 
 It works!
 
-@author: Robin
+@author: Sleggosaurus rex, da ørkenen var øde pushet sleggosaur til hub og gjorde liv i systemet
 """
 import numpy as np
 import array as arr
@@ -12,13 +12,15 @@ import time as time
 import matplotlib.pyplot as plt
 
 
-
+#skal være en funskjon som aktiverer porten til motor A
 def Amove(z):
     print (z ,"| A motor steps")
-    
+
+#skal være en funskjon som aktiverer porten til motor B
 def Bmove(z):
     print (z ,"| B motor steps")
 
+#matte greiær
 def deltaA(delX, delY):
     return delX + delY
 
@@ -27,6 +29,7 @@ def deltaB(delX, delY):
 
 x = 5
 
+#Printer all type informasjon
 def printall(x,y,nx,ny,xd,yd):
     print("")
     print("---------------------")
@@ -43,36 +46,43 @@ def printall(x,y,nx,ny,xd,yd):
 
 
 def start():
+    #lager variabler for start/current posisjon av x og y
     position_y = 0
     position_x = 0
-    
+
+    #lager variabel for den posisjonen den skal til
     new_position_x = 0
     new_position_y = 0
-    
+
+    #variabel for antall steps motoren skal ta
     motorAmovement = 0
     motorBmovement = 0
     
-    
+    #lager variablel for forskjellen på ny posisjon og nåværende posisjon
     position_x_diff = new_position_x - position_x
     position_y_diff = new_position_y - position_y
     
     for i in range(x):
-            
+            #lager tilfeldig kordinat i felt på 40x40
             new_position_x = np.random.randint(0,40)
             new_position_y = np.random.randint(0,40)
+
+            #oppdaterer position_diff og printer info
             position_x_diff = new_position_x - position_x
             position_y_diff = new_position_y - position_y
             printall(position_x , position_y, new_position_x, new_position_y, position_x_diff, position_y_diff)
-            
+
+            #bruker matte greiær for å mattifisere til hvor mange steps motoren må ta
             motorAmovement = deltaA(position_x_diff, position_y_diff)
             motorBmovement = deltaB(position_x_diff, position_y_diff)
-            
+    
             Amove(motorAmovement)
             Bmove(motorBmovement)
             print ("")
             print ("-----------------")
             print ("")
-                
+
+            #setter den posisjonen den skulle til, til nåværende posisjon
             position_x = new_position_x
             position_y = new_position_y
             time.sleep(1)
